8b541c3f599fa5974546bf0c47c2554c78566dfc
gusfrei <gustavorfrei@gmail.com>
2023-07-27 13:18:55 -0400

    Update authentication

---
+++ server/models/passport-config.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const User = require('./models/user'); // Adjust the path if needed
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, async (err, user) => {
+      if (err) return done(err);
+      if (!user) return done(null, false, { message: 'Incorrect username.' });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return done(null, false, { message: 'Incorrect password.' });
+
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,8 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user')
+const bcrypt = require('bcryptjs');
+
 // modules required for routing
 let express = require('express');
 let router = express.Router();
@@ -12,6 +17,68 @@ router.get('/', (req, res, next) => {
     title: 'Home',
     incidents: ''
    });
+});
+
+// GET login page
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Login' });
+});
+
+// POST login form submission
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/incidents',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+// GET logout route
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+// POST user registration form submission
+router.post('/register', (req, res, next) => {
+  const { username, password } = req.body;
+
+  // Check if the username is already taken
+  User.findOne({ username: username }, async (err, existingUser) => {
+    if (err) return next(err);
+
+    if (existingUser) {
+      // If the username is already taken, render the registration form again with an error message
+      return res.render('register', {
+        title: 'Register',
+        errorMessage: 'Username already taken. Please choose a different username.',
+      });
+    }
+
+    try {
+      // Hash the password before saving it to the database
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create a new user object with the provided username and hashed password
+      const newUser = new User({
+        username: username,
+        password: hashedPassword,
+      });
+
+      // Save the new user to the database
+      newUser.save((err) => {
+        if (err) return next(err);
+
+        // Redirect to the login page after successful registration
+        res.redirect('/login');
+      });
+    } catch (error) {
+      return next(error);
+    }
+  });
+});
+
+// GET user registration page
+router.get('/register', (req, res) => {
+  res.render('register', { title: 'Register', errorMessage: '' });
 });
 
 module.exports = router;
